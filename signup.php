f12bc4afc9ddf56a9cfa7957ddac2244f168d60e
SRHOTEL <106994377+SRHOTEL@users.noreply.github.com>
2022-06-08 10:33:45 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,52 @@
+
+ <Html>  
+<head>   
+<title>  
+Registration Page  
+</title>  
+</head>  
+<body bgcolor="White">
+
+
+<br>  
+<br>  
+<form1>  
+
+  
+<label> Firstname </label>         
+<input type="text" name="firstname" size="15"/> <br> <br>  
+<label> Lastname: </label>         
+<input type="text" name="lastname" size="15"/> <br> <br>  
+  
+</select>  
+  
+<br>  
+<br>  
+<label>   
+Gender :  
+</label><br>  
+<input type="radio" name="male"/> Male <br>  
+<input type="radio" name="female"/> Female <br>  
+
+<br>  
+<br>  
+  
+<label>   
+Phone :  
+</label>  
+<input type="text" name="country code"  value="+91" size="2"/>   
+<input type="text" name="phone" size="10"/> <br> <br>   
+Email:  
+<input type="email" id="email" name="email"/> <br>    
+<br> <br>  
+Password:  
+<input type="Password" id="pass" name="pass"> <br>   
+<br> <br>  
+Confirm password:  
+<input type="Password" id="repass" name="repass"> <br> <br> 
+<form action="registration.html" class="container"><br>
+<button type="submit" formaction="newfood.html" class="btn1">submit</button><br><br>
+  
+</form>  
+</body>  
+</html> 
